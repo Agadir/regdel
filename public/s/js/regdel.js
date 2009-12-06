fb31b543438bc1da0230733e05329e486ec66b1e
Albert Lash <albert.lash@docunext.com>
2009-12-06 19:17:25 +0000

    Javascript starting to get messy now, must be a better way to unite Ruby objects and Javascript objects

--- public/s/js/regdel.js
+++ public/s/js/regdel.js
@@ -110,11 +110,13 @@ $('document').ready(function() {
 function update_journal_entry_form(myid) {
   
   $.getJSON("/json/entry/"+myid, function(data) {
+    $("#memorandum").val(data.memorandum);
+    $("#entry_datetime").val(data.entered_on);
     $(".credit-row:not(:first)").remove();
     var i = 0;
     $.each(data.credits, function(i, item) {
         if(i>=1) {
-          $(".credit-row:first").clone().appendTo("#entry_form_table tbody");
+          $(".credit-row:first").clone().appendTo("#journal-entry-amounts tbody");
         }
         $(".credit-row:eq("+i+") input").val(item.amount);
         i++;
@@ -124,7 +126,7 @@ function update_journal_entry_form(myid) {
     var i = 0;
     $.each(data.debits, function(i, item) {
         if(i>=1) {
-        $(".debit-row:first").clone().appendTo("#entry_form_table tbody");
+        $(".debit-row:first").clone().appendTo("#journal-entry-amounts tbody");
         }
         $(".debit-row:eq("+i+") input").val(item.amount);
         i++;
