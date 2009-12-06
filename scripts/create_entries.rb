827405d69a91fe4a074f6cc83ec0459d077364a2
Albert Lash <albert.lash@docunext.com>
2009-12-06 02:00:16 +0000

    Script to populate db with entries

---
+++ scripts/create_entries.rb
@@ -0,0 +1,29 @@
+require 'rubygems'
+require 'sinatra'
+require 'builder'
+require 'bigdecimal'
+require 'bigdecimal/util'
+require 'dm-core'
+require 'dm-validations'
+require 'dm-timestamps'
+require 'dm-serializer'
+require 'dm-aggregates'
+require 'xml/libxml'
+require 'xml/libxslt'
+require 'json'
+
+require 'data/regdel_dm'
+
+
+class RdMoney < String
+    def no_d
+        return (self.gsub(/[^0-9\.]/,'').to_d * 100).to_i
+    end
+end
+
+for i in 1..5
+    @entry = Entry.new(:memorandum => "Hi",:entered_on => Time.now.to_i)
+    @entry.save
+    @entry.credits.create(:amount => RdMoney.new("3.00").no_d)
+    @entry.debits.create(:amount => RdMoney.new("3.00").no_d)
+end
