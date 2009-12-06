e417f14607fe501763279cc08039818a491a5bf7
Albert Lash <albert.lash@docunext.com>
2009-12-06 23:34:26 +0000

    Sinatra sub-class is finally working right.

--- config.ru
+++ config.ru
@@ -3,6 +3,8 @@ require 'rack/utils'
 require 'xml/libxml'
 require 'xml/libxslt'
 require 'rexml/document'
+gem 'rack-rewrite', '~> 0.2.0'
+require 'rack-rewrite'
 
 require 'helpers/rack/xslview'
 require 'helpers/rack/nolength'
@@ -11,29 +13,30 @@ require 'helpers/rack/finalcontentlength'
 
 
 
-gem 'rack-rewrite', '~> 0.2.0'
-require 'rack-rewrite'
-use Rack::Rewrite do
-    rewrite '/entry/new', '/s/xhtml/entry_all_form.html'
-    rewrite %r{/entry/edit(.*)}, '/s/xhtml/entry_all_form.html'
-    rewrite %r{/account/new(.*)}, '/s/xhtml/account_form.html'
-    rewrite %r{/account/edit/(.*)}, '/s/xhtml/account_form.html?id=$1'
-    rewrite '/', '/s/xhtml/welcome.html'
+
+map "/" do
+
+  use Rack::Rewrite do
+      rewrite '/entry/new', '/s/xhtml/entry_all_form.html'
+      rewrite %r{/entry/edit(.*)}, '/s/xhtml/entry_all_form.html'
+      rewrite %r{/account/new(.*)}, '/s/xhtml/account_form.html'
+      rewrite %r{/account/edit/(.*)}, '/s/xhtml/account_form.html?id=$1'
+      rewrite '/', '/s/xhtml/welcome.html'
+  end
+  
+  
+  
+  xslt = ::XML::XSLT.new()
+  xslt.xsl = REXML::Document.new File.open('/var/www/dev/regdel/views/xsl/html_main.xsl')
+  
+
+
+  # These are processed in reverse order it seems
+  use Rack::CommonLogger
+  use Rack::FinalContentLength
+  use Rack::XSLView, :myxsl => xslt
+  #use Rack::ExampleA
+  use Rack::NoLength
+  run Regdel::Main
+  #app = Sinatra::Application
 end
-
-
-
-xslt = ::XML::XSLT.new()
-xslt.xsl = REXML::Document.new File.open('/var/www/dev/regdel/views/xsl/html_main.xsl')
-
-# These are processed in reverse order it seems
-use Rack::FinalContentLength
-use Rack::XSLView, :myxsl => xslt
-#use Rack::ExampleA
-use Rack::NoLength
-
-
-app = Regdel
-#app = Sinatra::Application
-
-run app
--- regdel.rb
+++ regdel.rb
@@ -25,206 +25,208 @@ class RdMoney < String
     end
 end
 
-class Regdel < Sinatra::Base
-  helpers Sinatra::XSLView
-  
-  set :views, File.dirname(__FILE__) + '/views'
-  set :public, File.dirname(__FILE__) + '/public'
-  set :dump_errors, false
-  set :raise_errors, true
-  set :pagination, 10
-  
-  
-  get '/accounts' do
-      @accounts = Account.all(:closed_on => 0)
-      accounts = builder :'xml/accounts'
-      xslview accounts, '/var/www/dev/regdel/views/xsl/accounts.xsl'
-  end
-  
-  get '/json/account/:id' do
-      content_type :json
-      Account.get(params[:id]).to_json
-  end
-  
-  post '/account/submit' do
-      if params[:id]
-          @account = Account.get(params[:id])
-          @account.attributes = {
-              :name => params[:name],
-              :type_id => params[:type_id],
-              :number => params[:number],
-              :description => params[:description],
-              :hide => params[:hide]
-          }
-          error_target = '/account/edit/' + params[:id]
-      else
-          @account = Account.new(
-              :name => params[:name],
-              :type_id => params[:type_id],
-              :number => params[:number],
-              :description => params[:description],
-              :hide => params[:hide]
-          )
-          error_target = '/account/new'
-      end
-  
-      if @account.save
-        redirect '/accounts'
-      else
-        myerrors = ""
-        @account.errors.each do |e|
-            myerrors << e.to_s
+module Regdel
+  class Main < Sinatra::Base
+    helpers Sinatra::XSLView
+    set :static, true
+    set :views, File.dirname(__FILE__) + '/views'
+    set :public, File.dirname(__FILE__) + '/public'
+    set :dump_errors, false
+    set :raise_errors, true
+    set :pagination, 10
+    
+    
+    get '/accounts' do
+        @accounts = Account.all(:closed_on => 0)
+        accounts = builder :'xml/accounts'
+        xslview accounts, '/var/www/dev/regdel/views/xsl/accounts.xsl'
+    end
+    
+    get '/json/account/:id' do
+        content_type :json
+        Account.get(params[:id]).to_json
+    end
+    
+    post '/account/submit' do
+        if params[:id]
+            @account = Account.get(params[:id])
+            @account.attributes = {
+                :name => params[:name],
+                :type_id => params[:type_id],
+                :number => params[:number],
+                :description => params[:description],
+                :hide => params[:hide]
+            }
+            error_target = '/account/edit/' + params[:id]
+        else
+            @account = Account.new(
+                :name => params[:name],
+                :type_id => params[:type_id],
+                :number => params[:number],
+                :description => params[:description],
+                :hide => params[:hide]
+            )
+            error_target = '/account/new'
         end
-        redirect error_target + '?error=' + myerrors
-      end
-  end
-  
-  post '/account/close' do
-      content_type 'application/xml', :charset => 'utf-8'
-      @account = Account.get(params[:id])
-      @account.attributes = {
-          :closed_on => Time.now.to_i
-      }
-      if @account.save
-          "<success>Success</success>"
-      else
-        myerrors = ""
-        @account.errors.each do |e|
-            myerrors << e.to_s
+    
+        if @account.save
+          redirect '/accounts'
+        else
+          myerrors = ""
+          @account.errors.each do |e|
+              myerrors << e.to_s
+          end
+          redirect error_target + '?error=' + myerrors
         end
-      end
-  end
-  
-  post '/entry/submit' do
-      @entry = Entry.new(:memorandum => params[:memorandum])
-      @entry.save
-      @entry.credits.create(:amount => RdMoney.new(params[:credit_amount]).no_d, :account_id => params[:credit_account_id])
-      @entry.debits.create(:amount => RdMoney.new(params[:debit_amount]).no_d, :account_id => params[:debit_account_id])
-      redirect '/journal'
-  end
-  
-  get '/json/entry/:id' do
-      content_type :json
-      Entry.get(params[:id]).to_json(:methods => [:credits,:debits])
-  end
-  get '/journal' do
-      redirect '/journal/0'
-  end
-  get '/journal/:offset' do
-      count = Entry.count()
-      myoffset = params[:offset].to_i
-      incr = options.pagination
-  
-      @myentries = Entry.all(:limit => options.pagination, :offset => myoffset)
-      @prev = (myoffset - incr) < 0 ? 0 : myoffset - incr
-      @next = myoffset + incr > count ? myoffset : myoffset + incr
-      entries = builder :'xml/entries'
-      xslview entries, '/var/www/dev/regdel/views/xsl/entries_simpler.xsl'
-  end
-  get '/journal/full' do
-      @myentries = Entry.all
-      entries = builder :'xml/entries'
-      xslview entries, '/var/www/dev/regdel/views/xsl/entries.xsl'
-  end
-  
-  get '/ledger' do
-      @myentries = Entry.all
-      entries = builder :'xml/entries'
-      xslview entries, '/var/www/dev/regdel/views/xsl/entries.xsl'
-  end
-  
-  get '/stylesheet.css' do
-    content_type 'text/css', :charset => 'utf-8'
-    sass :'css/regdel'
-  end
-  
-  error do
-    'So what happened was...' + request.env['sinatra.error'].message
-  end
-  
-  not_found do
-      'This is nowhere to be found'
-  end
-      
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  # TESTS
-  
-  
-  get '/raw/entries' do
-      content_type 'application/xml', :charset => 'utf-8'
-      @myentries = Entry.all
-      builder :'xml/entries'
-  end
-  get '/raw/account/select' do
-      content_type 'application/xml', :charset => 'utf-8'
-      @accounts = Account.all(:closed_on => 0)
-      builder :'xml/account_select'
-  end
-  get '/raw/accounts' do
-      content_type 'application/xml', :charset => 'utf-8'
-      @accounts = Account.all(:closed_on => 0)
-      builder :'xml/accounts'
-  end
-  get '/raw/test' do
-      content_type 'application/xml', :charset => 'utf-8'
-      builder :'xml/test'
-  end
-  
-  
-  
-  get '/raw/test/entries' do
-      content_type 'application/xml', :charset => 'utf-8'
-      @myentries = Entry.all(:limit => 4, :offset => 0)
-      builder :'xml/entries_test_sum'
-  end
-  get '/raw/test/entries/:offset' do
-      content_type 'application/xml', :charset => 'utf-8'
-      @myentries = Entry.all(:limit => 4, :offset => params[:offset].to_i)
-      builder :'xml/entries_test_sum'
-  end
-  get '/raw/test/entries2' do
-      content_type 'application/xml', :charset => 'utf-8'
-      @myentries = Entry.all
-      @mycredits = Credit.all
-      @mydebits = Debit.all
-      builder :'xml/entries_test_big'
-  end
-  get '/raw/test/entries3' do
-      content_type 'application/xml', :charset => 'utf-8'
-      @myentries = Entry.all
-      builder :'xml/entries_test_sum'
-  end
-  
-  get '/raw/test/to_json' do
-      content_type :json
-      @myentries = Entry.all
-      @myentries.to_json()
-  end
-  get '/raw/test/to_jsonassoc' do
-      content_type :json
-      @myentries = Entry.all
-      @myentries.to_json(:methods => [:credits,:debits])
-  end
-  get '/raw/test/to_xml' do
-      content_type 'application/xml'
-      @myentries = Entry.all
-      @myentries.to_xml()
-  end
-  get '/raw/test/to_xmlassoc' do
-      content_type 'application/xml'
-      @myentries = Entry.all
-      @myentries.to_xml(:methods => [:credits,:debits])
+    end
+    
+    post '/account/close' do
+        content_type 'application/xml', :charset => 'utf-8'
+        @account = Account.get(params[:id])
+        @account.attributes = {
+            :closed_on => Time.now.to_i
+        }
+        if @account.save
+            "<success>Success</success>"
+        else
+          myerrors = ""
+          @account.errors.each do |e|
+              myerrors << e.to_s
+          end
+        end
+    end
+    
+    post '/entry/submit' do
+        @entry = Entry.new(:memorandum => params[:memorandum])
+        @entry.save
+        @entry.credits.create(:amount => RdMoney.new(params[:credit_amount]).no_d, :account_id => params[:credit_account_id])
+        @entry.debits.create(:amount => RdMoney.new(params[:debit_amount]).no_d, :account_id => params[:debit_account_id])
+        redirect '/journal'
+    end
+    
+    get '/json/entry/:id' do
+        content_type :json
+        Entry.get(params[:id]).to_json(:methods => [:credits,:debits])
+    end
+    get '/journal' do
+        redirect '/journal/0'
+    end
+    get '/journal/:offset' do
+        count = Entry.count()
+        myoffset = params[:offset].to_i
+        incr = options.pagination
+    
+        @myentries = Entry.all(:limit => options.pagination, :offset => myoffset)
+        @prev = (myoffset - incr) < 0 ? 0 : myoffset - incr
+        @next = myoffset + incr > count ? myoffset : myoffset + incr
+        entries = builder :'xml/entries'
+        xslview entries, '/var/www/dev/regdel/views/xsl/entries_simpler.xsl'
+    end
+    get '/journal/full' do
+        @myentries = Entry.all
+        entries = builder :'xml/entries'
+        xslview entries, '/var/www/dev/regdel/views/xsl/entries.xsl'
+    end
+    
+    get '/ledger' do
+        @myentries = Entry.all
+        entries = builder :'xml/entries'
+        xslview entries, '/var/www/dev/regdel/views/xsl/entries.xsl'
+    end
+    
+    get '/stylesheet.css' do
+      content_type 'text/css', :charset => 'utf-8'
+      sass :'css/regdel'
+    end
+    
+    error do
+      'So what happened was...' + request.env['sinatra.error'].message
+    end
+    
+    not_found do
+        'This is nowhere to be found'
+    end
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    # TESTS
+    
+    
+    get '/raw/entries' do
+        content_type 'application/xml', :charset => 'utf-8'
+        @myentries = Entry.all
+        builder :'xml/entries'
+    end
+    get '/raw/account/select' do
+        content_type 'application/xml', :charset => 'utf-8'
+        @accounts = Account.all(:closed_on => 0)
+        builder :'xml/account_select'
+    end
+    get '/raw/accounts' do
+        content_type 'application/xml', :charset => 'utf-8'
+        @accounts = Account.all(:closed_on => 0)
+        builder :'xml/accounts'
+    end
+    get '/raw/test' do
+        content_type 'application/xml', :charset => 'utf-8'
+        builder :'xml/test'
+    end
+    
+    
+    
+    get '/raw/test/entries' do
+        content_type 'application/xml', :charset => 'utf-8'
+        @myentries = Entry.all(:limit => 4, :offset => 0)
+        builder :'xml/entries_test_sum'
+    end
+    get '/raw/test/entries/:offset' do
+        content_type 'application/xml', :charset => 'utf-8'
+        @myentries = Entry.all(:limit => 4, :offset => params[:offset].to_i)
+        builder :'xml/entries_test_sum'
+    end
+    get '/raw/test/entries2' do
+        content_type 'application/xml', :charset => 'utf-8'
+        @myentries = Entry.all
+        @mycredits = Credit.all
+        @mydebits = Debit.all
+        builder :'xml/entries_test_big'
+    end
+    get '/raw/test/entries3' do
+        content_type 'application/xml', :charset => 'utf-8'
+        @myentries = Entry.all
+        builder :'xml/entries_test_sum'
+    end
+    
+    get '/raw/test/to_json' do
+        content_type :json
+        @myentries = Entry.all
+        @myentries.to_json()
+    end
+    get '/raw/test/to_jsonassoc' do
+        content_type :json
+        @myentries = Entry.all
+        @myentries.to_json(:methods => [:credits,:debits])
+    end
+    get '/raw/test/to_xml' do
+        content_type 'application/xml'
+        @myentries = Entry.all
+        @myentries.to_xml()
+    end
+    get '/raw/test/to_xmlassoc' do
+        content_type 'application/xml'
+        @myentries = Entry.all
+        @myentries.to_xml(:methods => [:credits,:debits])
+    end
   end
 end
