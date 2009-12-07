fda899f50bcde04a3fa6323b0eb12f6a393a725d
Albert Lash <albert.lash@docunext.com>
2009-12-07 14:21:13 +0000

    ramping up the amount of test data

--- scripts/create_entries.rb
+++ scripts/create_entries.rb
@@ -21,9 +21,10 @@ class RdMoney < String
     end
 end
 
-for i in 1..5
-    @entry = Entry.new(:memorandum => "Hi",:entered_on => Time.now.to_i)
+for i in 1..50
+    mycents = rand(8)
+    @entry = Entry.new(:memorandum => "Hi #{i}",:entered_on => Time.now.to_i)
     @entry.save
-    @entry.credits.create(:amount => RdMoney.new("#{i}.00").no_d, :account_id => 1)
-    @entry.debits.create(:amount => RdMoney.new("#{i}.00").no_d, :account_id => 2)
+    @entry.credits.create(:amount => RdMoney.new("#{i}.0#{mycents}").no_d, :account_id => 1)
+    @entry.debits.create(:amount => RdMoney.new("#{i}.0#{mycents}").no_d, :account_id => 2)
 end
