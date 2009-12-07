799e95ac9c5df146f78c9b7f051a387a710f9bc9
Albert Lash <albert.lash@docunext.com>
2009-12-07 00:00:24 +0000

    Entry edit form

--- public/s/js/entry_form.js
+++ public/s/js/entry_form.js
@@ -14,3 +14,37 @@ $('document').ready(function() {
   }
 
 });
+
+
+function update_journal_entry_form(myid) {
+  
+  $.getJSON("/json/entry/"+myid, function(data) {
+    $("#memorandum").val(data.memorandum);
+    $("#entry_datetime").val(data.entered_on);
+    $(".credit-row:not(:first)").remove();
+    var i = 0;
+    $.each(data.credits, function(i, item) {
+        if(i>=1) {
+          $(".credit-row:first").clone().appendTo("#journal-entry-amounts tbody");
+        }
+        $(".credit-row:eq("+i+") input").val(item.amount);
+        $(".credit-row:eq("+i+") select").val(item.account_id);
+        i++;
+    });
+
+    $(".debit-row:not(:first)").remove();
+    var i = 0;
+    $.each(data.debits, function(i, item) {
+        if(i>=1) {
+        $(".debit-row:first").clone().appendTo("#journal-entry-amounts tbody");
+        }
+        $(".debit-row:eq("+i+") input").val(item.amount);
+        $(".debit-row:eq("+i+") select").val(item.account_id);
+        i++;
+    });
+    $('input').unbind("click");
+    $('input').one("click",function() {
+        update_journal_entry_form(myid);
+    });
+  });
+}
--- public/s/js/regdel.js
+++ public/s/js/regdel.js
@@ -25,33 +25,3 @@ $('document').ready(function() {
 
 
 });
-
-function update_journal_entry_form(myid) {
-  
-  $.getJSON("/json/entry/"+myid, function(data) {
-    $("#memorandum").val(data.memorandum);
-    $("#entry_datetime").val(data.entered_on);
-    $(".credit-row:not(:first)").remove();
-    var i = 0;
-    $.each(data.credits, function(i, item) {
-        if(i>=1) {
-          $(".credit-row:first").clone().appendTo("#journal-entry-amounts tbody");
-        }
-        $(".credit-row:eq("+i+") input").val(item.amount);
-        i++;
-    });
-
-    $(".debit-row:not(:first)").remove();
-    var i = 0;
-    $.each(data.debits, function(i, item) {
-        if(i>=1) {
-        $(".debit-row:first").clone().appendTo("#journal-entry-amounts tbody");
-        }
-        $(".debit-row:eq("+i+") input").val(item.amount);
-        i++;
-    });
-    $('input').click(function() {
-        update_journal_entry_form(myid);
-    });
-  });
-}
