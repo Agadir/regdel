d772aeaf576b228ca9787b904b8d4f05d31d39d8
Albert Lash <albert.lash@docunext.com>
2009-12-07 11:47:40 +0000

    organizing scripts and such

--- scripts/account_types_to_json.sh
+++ scripts/account_types_to_json.sh
@@ -1,1 +1,1 @@
-xsltproc views/xsl/account_types_to_json.xsl public/s/xml/raw/account_types.xml > public/s/js/account_types.json
+xsltproc --stringparam format json views/xsl/account_types2many.xsl public/s/xml/raw/account_types.xml > public/s/js/account_types.json
---
+++ scripts/account_types_to_ruby.sh
@@ -0,0 +1,1 @@
+xsltproc --stringparam format ruby views/xsl/account_types2many.xsl public/s/xml/raw/account_types.xml > data/account_types.rb
