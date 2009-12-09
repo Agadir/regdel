2111249e2111e3db1ff9116e98970752ea51cfb8
Albert Lash <albert.lash@docunext.com>
2009-12-09 03:08:19 +0000

    Javascript for ledger

---
+++ public/s/js/ledger.js
@@ -0,0 +1,7 @@
+/* Regdel Javascript */
+// Read a page's GET URL variables and return them as an associative array.
+
+$('document').ready(function() {
+  $("#nav-ledger").addClass("active");
+
+});
