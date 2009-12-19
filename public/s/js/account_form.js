8950de2ecf63c61d4133b988742e2d04093aa522
Albert Lash <albert.lash@docunext.com>
2009-12-19 21:20:40 +0000

    testing for account id

--- public/s/js/account_form.js
+++ public/s/js/account_form.js
@@ -10,16 +10,18 @@ $('document').ready(function() {
       loadUrl: "/s/xml/raw/account_types_select.xml",
   });
   var myid = jQuery.url.segment(2);
-  $.getJSON("/json/account/"+myid, function(data) {
-      $.each(data, function(i, item) {
-          if ($('#' + i).length) {
-              if($('#' + i).attr("type")=="checkbox") {
-                  $('#' + i).val([item]);
-              } else {
-                  $('#' + i).val(item);
-              }
-          }
-      });
-  });
+  if (myid > 0) {
+    $.getJSON("/json/account/"+myid, function(data) {
+        $.each(data, function(i, item) {
+            if ($('#' + i).length) {
+                if($('#' + i).attr("type")=="checkbox") {
+                    $('#' + i).val([item]);
+                } else {
+                    $('#' + i).val(item);
+                }
+            }
+        });
+    });
+  }
   $("form").append('<input type="hidden" name="id" value="' + myid +'" />');
 });
