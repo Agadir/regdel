fd239ac5801509829bd9f51fa22667bd46b19297
Albert Lash <albert.lash@docunext.com>
2009-12-19 21:49:43 +0000

    using jquery to set tablesorter

--- public/s/js/ledger.js
+++ public/s/js/ledger.js
@@ -1,6 +1,3 @@
-/* Regdel Javascript */
-// Read a page's GET URL variables and return them as an associative array.
-
 $('document').ready(function() {
 
   $("#ledger-table", $("#page-content")).addClass("tablesorter").tablesorter({
