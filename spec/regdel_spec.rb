5c886e1f558fd21d05c7c690c22f79b7c3ad26b4
Albert Lash <albert.lash@docunext.com>
2009-12-20 09:45:13 +0000

    Added more spec tests

--- spec/regdel_spec.rb
+++ spec/regdel_spec.rb
@@ -11,4 +11,18 @@ describe "Regdel" do
     get '/accounts'
     last_response.should be_ok
   end
+
+  it "should respond to /json/account/1" do
+    get '/json/account/1'
+    last_response.should be_ok
+  end
+  it "should respond to /ledger" do
+    get '/ledger'
+    last_response.should be_ok
+    last_response.body.should include("</html>")
+  end
+  it "should respond to /stylesheet.css" do
+    get '/stylesheet.css'
+    last_response.headers["Content-Type"].should == "text/css;charset=utf-8"
+  end
 end
