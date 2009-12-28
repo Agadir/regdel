01d78e4251df2e6af47c7017f017efb0e6726a24
Docunext <albert.lash@docunext.com>
2009-12-27 19:08:52 -0500

    for deployment

--- rund.sh
+++ rund.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cd /var/www/dev/regdel/
-exec /var/lib/gems/1.8/gems/unicorn-0.95.3/bin/unicorn --env none -l 3000
+cd /var/www/dev/regdel/current
+exec /var/lib/gems/1.9.1/gems/unicorn-0.95.3/bin/unicorn --env none -l 3000
