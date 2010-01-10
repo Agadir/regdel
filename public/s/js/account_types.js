27ea8e8178aa0b13ac6fbe53a3b8d451d262bb1e
Docunext <albert.lash@docunext.com>
2010-01-10 05:45:51 -0500

    Continuing to refactor. Special thanks to Martin Fowler's notes regarding Rake.

---
+++ public/s/js/account_types.js
@@ -0,0 +1,26 @@
+
+{ "select" : [ 
+
+{ "Value": "10000", "Text": "Assets"},
+
+{ "Value": "20000", "Text": "Liabilities"},
+
+{ "Value": "30000", "Text": "Stockholder's Equity"},
+
+{ "Value": "40000", "Text": "Revenues"},
+
+{ "Value": "50000", "Text": "Expenses"},
+
+{ "Value": "60000", "Text": "Gain"},
+
+{ "Value": "70000", "Text": "Loss"},
+
+{ "Value": "80000", "Text": "Distribution from Equity"},
+
+{ "Value": "90000", "Text": "Contribution to Equity"},
+
+{ "Value": "100000", "Text": "Comprehensive Income"},
+
+{ "Value": "110000", "Text": "Other"},
+
+] }
