b30c973a48b8ed8800c2bd75b649c18dd5034512
Docunext <albert.lash@docunext.com>
2010-01-27 22:30:56 -0500

    adding logging to demo

--- rund.sh
+++ rund.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /var/www/dev/regdel/current
-exec /var/lib/gems/1.9.1/gems/unicorn-0.95.3/bin/unicorn --env demo -l 3000
+exec /var/lib/gems/1.9.1/gems/unicorn-0.95.3/bin/unicorn -c /var/www/dev/regdel/current/config/unicorn.conf --env demo -l 3000
