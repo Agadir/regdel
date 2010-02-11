9823c49aba37cc1af8901efe8ed8bb9bb00e202e
Docunext <albert.lash@docunext.com>
2010-02-10 22:06:20 -0500

    regdel

--- scripts/default_accounts.rb
+++ scripts/default_accounts.rb
@@ -21,6 +21,7 @@
 # or write to the Free Software Foundation, Inc., 51 Franklin Street,
 # Fifth Floor, Boston, MA 02110-1301 USA
 ##
+require 'data/regdel-dm-modules'
 require 'data/regdel_dm'
 
 Account.new(
