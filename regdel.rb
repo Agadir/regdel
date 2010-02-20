e78e32b98db949a6e8824a789e9da0126655db4a
Docunext <albert.lash@docunext.com>
2010-02-19 21:01:33 -0500

    do not rebuild ledger on demo, well try again

--- regdel.rb
+++ regdel.rb
@@ -147,7 +147,7 @@ module Regdel
 
       # POSTs indicate data alterations, rebuild cache and semi-dynamic database entries
       if request.env['REQUEST_METHOD'].upcase == 'POST'
-        unless env['RACK_ENV']=='demo'
+        unless env['RACK_ENV']=='demoz'
           rebuild_ledger(Regdel.dirpfx + '/public/d/xhtml/ledger.html')
         end
         Account.all.each do |myaccount|
