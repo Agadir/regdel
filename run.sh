911c1721d33a9397c828beebd232eec16e387b08
Docunext <albert.lash@docunext.com>
2010-02-24 07:35:11 -0500

    updated unicorn

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 touch public/s/xhtml/*
 touch public/d/xhtml/*
 #rake files:welcome_html
-/var/lib/gems/1.8/gems/unicorn-0.95.3/bin/unicorn -c /var/www/dev/regdel/config/unicorn.dev.conf --env development -l 3000
+/var/lib/gems/1.9.1/bin/unicorn -c /var/www/dev/regdel/config/unicorn.dev.conf --env development -l 3000
 
