7620a4d84ecf09277b74c456fcdf435e66883497
Docunext <albert.lash@docunext.com>
2010-08-06 22:27:46 -0400

    using softlimit

--- rund.sh
+++ rund.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /var/www/dev/regdel/current
-exec /var/lib/gems/1.9.1/gems/unicorn-0.95.3/bin/unicorn -c /var/www/dev/regdel/current/config/unicorn.conf --env demo -l 3000
+exec softlimit -m 131457280 -o 100 exec /var/lib/gems/1.9.1/gems/unicorn-0.95.3/bin/unicorn -c /var/www/dev/regdel/current/config/unicorn.conf --env demo -l 3000
