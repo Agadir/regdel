043382b81bbe47afb94ef10ba5d3d153daaff0c9
docunext <albert.lash@docunext.com>
2011-07-01 23:55:36 -0400

    more additions

---
+++ db/migrate/20110702034504_create_accounts.rb
@@ -0,0 +1,15 @@
+class CreateAccounts < ActiveRecord::Migration
+  def self.up
+    create_table :accounts do |t|
+      t.string :name
+      t.integer :parent_id
+      t.string :type
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :accounts
+  end
+end
