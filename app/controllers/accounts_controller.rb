3467d75bb108c6e6507a093b736b23c4ff197e3c
docunext <albert.lash@docunext.com>
2011-07-02 00:46:05 -0400

    working on accounts

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -5,9 +5,11 @@ class AccountsController < ApplicationController
   end
 
   def show
+    @account = Account.find(params[:id])
   end
 
   def edit
+    @account = Account.find(params[:id])
   end
 
   def update
