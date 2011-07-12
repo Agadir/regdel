b635ad122426e7332a9c09171855b0a8e5d862b7
docunext <albert.lash@docunext.com>
2011-07-11 20:34:37 -0400

    validation

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -5,6 +5,10 @@ class Entry < ActiveRecord::Base
   has_many :accounts, :through => :entry_amounts
   accepts_nested_attributes_for :credits
   accepts_nested_attributes_for :debits
+
+  validates_size_of :credits, :minimum => 1
+  validates_size_of :debits, :minimum => 1
+
   def destroy
     raise ActiveRecord::IndestructibleRecord
   end
