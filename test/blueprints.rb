85aab94a3fefa2914f843ba42ca6178a4dbefd93
docunext <albert.lash@docunext.com>
2011-07-21 20:27:20 -0400

    mini

--- test/blueprints.rb
+++ test/blueprints.rb
@@ -4,9 +4,18 @@ Asset.blueprint do
   name { "Photo Copier" }
 end
 
+Revenue.blueprint do
+  name { "Hosting" }
+end
+
+Expense.blueprint do
+  name { "Electricity" }
+end
+
 Debit.blueprint do
   amount_in_cents { 123 }
 end
+
 Credit.blueprint do
   amount_in_cents { 123 }
 end
@@ -14,4 +23,8 @@ end
 Check.blueprint do
   memo { 'Payment to someone' }
   date { Time.now }
+end
+
+Invoice.blueprint do
+
 end
