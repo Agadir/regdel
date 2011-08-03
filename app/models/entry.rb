be84a8a17f2f1cb248f82f78c4addd2ef4bb9a6a
docunext <albert.lash@docunext.com>
2011-08-03 18:13:39 -0400

    fixed validators, still needs improvement

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,9 +2,11 @@ class Entry < ActiveRecord::Base
   has_many :entry_amounts
   has_many :credits
   has_many :debits
-  has_many :accounts, :through => :entry_amounts, :polymorphic => true
+  has_many :accounts, :through => :entry_amounts
+
   accepts_nested_attributes_for :credits
   accepts_nested_attributes_for :debits
+  accepts_nested_attributes_for :entry_amounts
 
   validate :credits_and_debits_must_balance, :if => :posted?
   validate :account_types_valid?
