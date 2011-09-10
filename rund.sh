12f95e6035edcaa8f89ff8ba97b1889e6d66459f
docunext <albert.lash@docunext.com>
2011-09-10 18:55:21 -0400

    bundle exec

--- rund.sh
+++ rund.sh
@@ -1,3 +1,3 @@
 !/bin/sh
 cd /var/www/dev/regdel/current
-exec softlimit -m 131457280 -o 100 bundle unicorn --env demo -l 3000
+exec softlimit -m 131457280 -o 100 bundle exec unicorn --env demo -l 3000
