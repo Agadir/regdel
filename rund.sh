3f8161eed09142307aa01f9f51f772ebec6f9e9e
docunext <albert.lash@docunext.com>
2011-09-18 00:31:10 -0400

    daemontools doesn't like rvm exec

--- rund.sh
+++ rund.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /var/www/dev/regdel/current
-exec softlimit -m 131457280 -o 100 /home/app/.rvm/bin/rvm exec bundle exec unicorn -c /var/www/dev/regdel/current/config/unicorn.conf  --env demo -l 3000
+exec softlimit -m 131457280 -o 100 "/home/app/.rvm/bin/rvm exec bundle exec unicorn -c /var/www/dev/regdel/current/config/unicorn.conf  --env demo -l 3000"
